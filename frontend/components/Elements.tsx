c55f53eb61ff79e01455edbf55aff33169c9703f
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-07-28 11:44:18 -0600

    moving Logo to the Elements and adding new elements

--- frontend/components/Elements.tsx
+++ frontend/components/Elements.tsx
@@ -1,4 +1,10 @@
-import React, { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+export const Logo = () => {
+  return (
+    <div className='min-w-[12rem] h-[2rem] bg-no-repeat bg-content bg-center' style={{ backgroundImage: 'url(/img/logo.svg)' }}></div>
+  )
+}
 
 export const HrDashed = () => {
   return <>
@@ -6,13 +12,114 @@ export const HrDashed = () => {
   </>
 }
 
-interface Header2Props {
+interface HeaderProps {
   children: ReactNode;
   className?: string;
 }
 
-export const Header2: React.FC<Header2Props> = ({ children, className }) => {
+export const Header1: React.FC<HeaderProps> = ({ children, className }) => {
   return (
-    <h2 className={`text-2xl card-title $`}>{children}</h2>
+    <h1
+      className={`  tracking-wide text-5xl font-bold text-left text-titles text-center ${className}`}
+    >
+      {children}
+    </h1>
   );
+};
+
+export const Header2: React.FC<HeaderProps> = ({ children, className }) => {
+  return (
+    <h2 className={` tracking-wide text-2xl font-bold text-center ${className}`}>{children}</h2>
+  );
+};
+
+export const HeaderText: React.FC<HeaderProps> = ({ children, className }) => {
+  return (
+    <p className={`text-xl tracking-wide leading-7 ${className}`}>{children}</p>)
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color: 'primary' | 'secondary' | 'tertiary';
+  children: ReactNode;
+}
+
+export const Btn: React.FC<ButtonProps> = ({ color = 'primary', children, ...rest }) => {
+  const defBtn = (color: string) => {
+    let btnClasses = 'mb-[1.5rem] btn text-base w-[290px] h-[25px]';
+
+    const PRIMARY = 'primary';
+    const SECONDARY = 'secondary';
+    const TERTIARY = 'tertiary';
+
+    type Action = {
+      condition: () => boolean;
+      action: () => string;
+    };
+
+    const actions: Action[] = [
+      {
+        condition: () => color === PRIMARY,
+        action: () => btnClasses += ' bg-primJr text-lightJr border-none hover:bg-hoverBtn disabled:bg-disabledBtn',
+      },
+      {
+        condition: () => color === SECONDARY,
+        action: () => btnClasses += ' bg-secJr text-lightJr border-none hover:bg-primJr',
+      },
+      {
+        condition: () => color === TERTIARY,
+        action: () => btnClasses += ' text-outlineBtn border-outlineBtn btn-outline hover:text-lightJr hover:bg-hoverBtn',
+      },
+    ];
+
+    const action = actions.find(({ condition }) => condition());
+
+    if (action) return action.action();
+
+    console.warn('Cannot assign correct styles to button');
+
+    return `${btnClasses}`;
+  }
+
+  return (
+    <button className={defBtn(color)} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+type SelectProps = {
+  className?: string;
+  label?: string;
+  options: string[];
+};
+
+export const Select: React.FC<SelectProps> = ({ className = 'w-[472px]', label, options }) => {
+  const textStyle = 'trackng-wide leading-7';
+
+  return <label className={`${className} ${textStyle} flex flex-col mb-5 gap-y-4`}>
+    {label && <span className='text-base text-center'>{label}</span>}
+
+    <select defaultValue={options[0]} className='mb-10 text-sm border-black ps-8 bg-disabledBtn hover:bg-hoverBtn active:bg-lightJr select select-bordered'>
+      {
+        options.map((string: string, i: number) => (<option disabled={i === 0} className='text-sm group-hover:bg-gray-100' key={i}>{string}</option>))
+      }
+    </select>
+  </label>
+};
+
+type InputProps = {
+  className?: string;
+  label?: string;
+  placeholder?: string;
+  type?: string;
+};
+
+export const InputField: React.FC<InputProps> = ({ className = 'w-[472px]', type = 'text', label, placeholder = '' }) => {
+  const textStyle = 'trackng-wide leading-7';
+
+  return <label className={`${className} flex flex-col mb-5 gap-y-4`}>
+    {label && <span className='text-base text-center'>{label}</span>}
+
+    <input type={type} placeholder={placeholder} className='mb-10 text-sm border-black ps-8 hover:bg-hoverBtn bg-lightJr input input-bordered' />
+  </label>
 };
--- frontend/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-const Logo = () => {
-  return (
-    <div className='min-w-[12rem] h-[2rem] bg-no-repeat bg-content bg-center' style={{ backgroundImage: 'url(/img/logo.svg)' }}></div>
-  )
-}
-
-export default Logo;
