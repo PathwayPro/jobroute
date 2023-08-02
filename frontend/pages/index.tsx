352e6529c4e63132f66273dbe19855e9b123b27b
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-08-01 23:56:54 -0600

    updating pages/roadmap and pages/index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,33 @@
-import Body from "@/components/Body";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Body from '@/components/Body';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import { apiMain } from '@/tools/routes';
 
 import { Inter } from 'next/font/google'
+import { MainProps } from '@/types/PropsTypes';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home: React.FC<MainProps> = (props) => {
   return (
     <div className='min-h-screen grid grid-cols-12'>
       <Header />
-      <Body />
+      <Body provinces={props.provinces} searchBy={props.searchBy} />
       <Footer />
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  const response = await fetch(apiMain);
+  const data = await response.json();
+
+  return {
+    props: {
+      provinces: data.provinces,
+      searchBy: data.searchby,
+    },
+  };
+}
+
+export default Home;
