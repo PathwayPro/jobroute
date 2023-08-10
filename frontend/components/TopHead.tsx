ca20f38f7079b346d403b5e9b7cfa4f4f97faa72
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-08-10 00:26:22 -0600

    fixed the issue of the horizontal scroll bar appear when no need.

--- frontend/components/TopHead.tsx
+++ frontend/components/TopHead.tsx
@@ -2,7 +2,7 @@ import { Logo } from './Elements';
 
 const TopHead = () => {
   return (
-    <div className='w-screen h-[4rem] flex justify-center items-center bg-primary'>
+    <div className='w-[100%] h-[4rem] flex justify-center items-center bg-primary'>
       <Logo />
     </div>
   )
