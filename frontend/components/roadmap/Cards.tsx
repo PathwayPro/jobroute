ca2fe5104212e040d99fae85945ad72d85592b49
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-08-10 00:07:07 -0600

    Refactoring of Cards component

--- frontend/components/roadmap/Cards.tsx
+++ frontend/components/roadmap/Cards.tsx
@@ -1,12 +1,12 @@
 import { ActionString, Field, RoadmapItem, RoadmapProps } from '@/types/PropsTypes';
 import Image from 'next/image';
 
-const EDUCATION = 'Education / Training';
-const EXPERIENCE = 'Experience / Projects';
-const LICENSING = 'Licensing / Certification';
-const NETWORKING = 'Job Search / Networking';
-
 const defIcon = (title: string): string => {
+  const EDUCATION = 'Education / Training';
+  const EXPERIENCE = 'Experience / Projects';
+  const LICENSING = 'Licensing / Certification';
+  const NETWORKING = 'Job Search / Networking';
+
   const actions: ActionString[] = [
     {
       condition: () => title === EDUCATION,
@@ -35,25 +35,33 @@ const defIcon = (title: string): string => {
   return '/img/networking.svg';
 };
 
+const ListItem: React.FC<{ field: Field }> = ({ field }) => {
+  return <li>
+    <span>{field.title}: </span>
+    <span>{field.desc}</span>
+  </li>
+}
+
+const Card: React.FC<{ card: RoadmapItem }> = ({ card }) => {
+  return <div className='relative flex-grow flex-shrink-0 pt-3 pl-8 ml-5 border-2 bg-hover-input'>
+    <span className='card-title'>{card.title}</span>
+    <Image className='absolute left-[-60px] top-[50%] transform -translate-y-1/2'
+      src={defIcon(card.title)} width={110} height={90} alt='card icons' />
+    <ol className='list-decimal list-outside ms-3 card-body'>
+      {card.content.map((field: Field, i: number) => {
+        return <ListItem field={field} key={i} />
+      })}
+    </ol>
+  </div>
+}
+
 const Cards = ({ cards }: Pick<RoadmapProps, 'cards'>) => {
 
   const cardsShow = (list: RoadmapItem[]) => {
     return <div className='flex flex-col h-full gap-8 col-span-11'>
       <div className='flex flex-col flex-grow gap-12'>
         {list.map((card: RoadmapItem, i: number) => {
-          return <div className='relative flex-grow flex-shrink-0 pt-3 pl-8 ml-5 border-2 bg-hover-input' key={i}>
-            <span className='card-title'>{card.title}</span>
-            <Image className='absolute left-[-60px] top-[50%] transform -translate-y-1/2'
-              src={defIcon(card.title)} width={110} height={90} alt='card icons' />
-            <ol className='list-decimal list-outside ms-3 card-body'>
-              {card.content.map((field: Field, i: number) => {
-                return <li key={i}>
-                  <span>{field.title}: </span>
-                  <span>{field.desc}</span>
-                </li>
-              })}
-            </ol>
-          </div>
+          return <Card card={card} key={i} />
         })}
       </div>
     </div>
