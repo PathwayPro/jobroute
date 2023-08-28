b0eeaa9dc9c219b2b8c4c0e3a3dce05f3e197d1e
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-08-28 09:27:52 -0600

    Wrapped App with the Context

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import React, { createContext } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const Context = createContext({});
+
+function App({ Component, pageProps }: AppProps) {
+  const sharedData = {
+    profession: '',
+    industry: '',
+    region: '',
+  };
+
+  return (
+    <Context.Provider value={sharedData}>
+      <Component {...pageProps} />
+    </Context.Provider>
+  );
 }
+
+export default App;
