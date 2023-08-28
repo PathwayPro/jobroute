05cffdfec3a932d6df7d45002293403c65ec1a41
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-08-28 00:02:16 -0600

    updating main route links

--- frontend/tools/routes.ts
+++ frontend/tools/routes.ts
@@ -1,3 +1,4 @@
 export const server = 'http://localhost:3000/api';
+export const extServer = 'http://127.0.0.1:8000'
 export const apiMain = `${server}/main`;
 export const apiRoadmap = `${server}/roadmap`;
