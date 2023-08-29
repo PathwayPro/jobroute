2c9ffaff8c143f27ee5a70a50ecffedc81aa90ea
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-08-29 12:12:15 -0600

    Added prompts and fetch functions

---
+++ frontend/fetch/fetchRoadmap.ts
@@ -0,0 +1,27 @@
+import { server } from "@/tools/routes";
+
+const fetchServerData = async (
+  endpoint: string,
+  profession: string,
+  industry: string,
+  province: string
+) => {
+  const url = `${server}/prompts?endpoint=${endpoint}&profession=${profession}&industry=${industry}&province=${province}`;
+
+  try {
+    const response = await fetch(url);
+
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return await response.json();
+
+  } catch (error) {
+console.error('Error in fetchServerData:', error);
+    throw error;
+  }
+}
+
+export default fetchServerData;
