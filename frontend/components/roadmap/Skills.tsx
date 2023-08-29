6e26d6a492c71294536454a93966face712c35d0
Elijah Lasovy <95259793+ocnm@users.noreply.github.com>
2023-08-29 12:04:22 -0600

    updating Skills comp

--- frontend/components/roadmap/Skills.tsx
+++ frontend/components/roadmap/Skills.tsx
@@ -3,13 +3,13 @@ import { Header3, HrDashed } from "../Elements";
 
 const Skills = ({skills}: Pick<RoadmapProps, 'skills'>) => {
   return <div className='mt-12'>
-    <HrDashed />
     <div className='mt-12 grid grid-cols-2 place-items-center'>
       {skills.map((skill: TitleContent, i: number) => {
         return <div key={i}>
           <Header3 className='text-primary-text'>{skill.title}</Header3>
           <ol className='list-decimal'>
             {skill.content.map((skill, i) => {
+            console.log(skill)
               return <li key={i}>{skill}</li>
             })}
           </ol>
