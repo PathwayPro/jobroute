7951bb0df2a69129b89dd0f85b7eb486fe2640c8
Bia Gavira <biagavirete@gmail.com>
2023-11-02 12:20:05 -0600

    Adjustments in some styles (responsive css)

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Logo } from './Elements';
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 z-5 w-full h-8 bg-white p-8 flex justify-between items-center'>
+    <div className='fixed top-0 z-10 w-full h-8 bg-white p-8 flex justify-between items-center'>
       <Link href='/'>
         <Logo variant="primary" />
       </Link>
