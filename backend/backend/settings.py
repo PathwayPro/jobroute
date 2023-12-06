88c7b54e9eae39c88e2b1de527bc3063e1da2237
Bia Gavira <biagavirete@gmail.com>
2023-12-05 19:18:34 -0700

    Update readme to include new environment variable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,17 +90,6 @@ ASGI_APPLICATION = 'backend.asgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-
-        #POSTGRES
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': os.environ.get("DB_NAME"),
-        # 'USER': os.environ.get("DB_USER"),
-        # 'PASSWORD':os.environ.get("DB_PASSWORD"),
-        # 'HOST': os.environ.get("DB_HOST"),
-        # 'PORT': os.environ.get("DB_PORT"),
-        #MySQL
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
